e3592ee2d5d4d519203b272699dda6c2df54dcc1
govoni <pietro.govoni@gmail.com>
2020-07-05 19:11:43 +0200

    funzioni

---
+++ Lezione_01/programmi/main_15.cpp
@@ -0,0 +1,30 @@
+/*
+c++ -o main_15 main_15.cpp
+*/
+
+#include <iostream>
+
+
+int raddoppia (int) ;
+
+
+int main (int arcg, char ** argv)
+  {
+  
+    for (int i = 0 ; i < 5 ; ++i)
+      {
+        std::cout << "il doppio di " << i << " vale: " << raddoppia (i) << std::endl ;
+      }
+    return 0 ;
+  }
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+int raddoppia (int input_value) 
+  {
+    return 2 * input_value ;
+  }
+
+
+
+
