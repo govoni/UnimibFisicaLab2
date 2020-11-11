8d51da96fb879facb39f82a77b933252a6f0c4a0
govoni <pietro.govoni@gmail.com>
2020-11-11 14:30:12 +0100

    cambiato tipo di ritorno

--- Lezione_06/Esercizi/esercizio05.cpp
+++ Lezione_06/Esercizi/esercizio05.cpp
@@ -3,13 +3,15 @@ Esercizio 05: Si implementi il metodo di integrazione hit-or-miss con la funzion
 Si scriva l'algoritmo che calcola l'integrale come una funzione esterna al programma main, facendo in modo che prenda come parametri di ingresso, oltre agli estremi lungo l'asse x e l'asse y, anche il numero di punti pseudo-casuali da generare.
 Si faccia in modo che l'algoritmo ritorni un contenitore contenente due elementi: il primo elemento sia il valore dell'integrale, il secondo sia la sua incertezza.
 
-c++ -o esercizio05 esercizio05.cpp 
+c++ -o esercizio05 esercizio05.cpp ../../Lezione_03/Esercizi/esercizio04/myarray.cc
 */
 
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+
+#include "../../Lezione_03/Esercizi/esercizio04/myarray.h"
 
 
 float rand_range (float min, float max)
@@ -23,8 +25,8 @@ double fsin (double x)
     return sin (x) ; 
   }
   
-void HitOrMiss (int Nrand, double g(double), double xMin, double xMax,
-              double yMin, double yMax, double* risultato )
+mioArray HitOrMiss (int Nrand, double g(double), double xMin, double xMax,
+                    double yMin, double yMax)
    {
      int nHit = 0;
      double area  = (xMax - xMin) * (yMax - yMin) ;
@@ -39,10 +41,11 @@ void HitOrMiss (int Nrand, double g(double), double xMin, double xMax,
      double p = nHit / static_cast<double> (Nrand) ;
      double varianza = area * area / static_cast<double> (Nrand) * p * (1. - p) ;
            
-     risultato[0] = nHit * area / static_cast<double> (Nrand) ;
-     risultato[1] = sqrt (varianza) ;
+     mioArray risultato (2) ;      
+     risultato.fill (0, nHit * area / static_cast<double> (Nrand)) ;
+     risultato.fill (1, sqrt (varianza)) ;
       
-    return;       
+    return risultato ;       
    }           
 
 //----------------- MAIN ----------------- 
@@ -55,13 +58,10 @@ int main (int argc, char ** argv)
   double x_max = M_PI ; 
   double y_min = 0. ; 
   double y_max = 1. ;
-  double* risultato = new double[2];
    
-  HitOrMiss(N,fsin,x_min,x_max,y_min,y_max,risultato);
+  mioArray risultato = HitOrMiss(N,fsin,x_min,x_max,y_min,y_max);
   
-  std::cout << "Integrale = " << risultato[0] << " +/- " << risultato[1] << std::endl ;
-            
-  delete[] risultato;          
+  std::cout << "Integrale = " << risultato.get1 (0) << " +/- " << risultato.get1 (1) << std::endl ;
             
   return 0 ;
 }
